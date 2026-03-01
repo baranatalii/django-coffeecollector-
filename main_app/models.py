691dc73c7364ee978dcce38edc8775c1c5ca05b0
Baranatalii <baranatalii@BARANs-MacBook-Air.local>
2026-02-28 21:24:40 -0500

    Models Serializers

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Coffee(models.Model): # <-- Check that the name is exactly 'Coffee'
+    name = models.CharField(max_length=100)
+    roast = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics 
+from .models import Coffee 
+from .serializers import CoffeeSerializer 
+
 
 class Home(APIView):
     def get(self, request):
         content = {'message': 'Welcome to the Coffee Collector API!'}
         return Response(content)
+
